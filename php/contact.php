796f4c01b8e8f901aae9fabab0c7362bfa4857a6
pentaice <117164228+pentaice@users.noreply.github.com>
2024-09-17 10:47:07 +0300

    son düzenleme img hariç

--- php/contact.php
+++ php/contact.php
@@ -38,8 +38,8 @@ if(get_magic_quotes_gpc()) {
 // Enter the email address that you want to emails to be sent to.
 // Example $address = "joe.doe@yourdomain.com";
 
-//$address = "example@example.net";
-$address = "example@example.net";
+//$address = "ddijitaleller@gmail.com";
+$address = "ddijitaleller@gmail.com";
 
 
 // Configuration option.
